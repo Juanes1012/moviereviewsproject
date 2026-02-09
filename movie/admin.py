ca8c793f670f97f5a9d97c756117396aaccfebf9
Juan Esteban Restrepo <jues06resto@gmail.com>
2026-02-09 00:03:42 -0500

    Implementación de visualización y búsqueda de películas en admin

--- movie/admin.py
+++ movie/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Movie
 
-# Register your models here.
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    search_fields = ['title']  # campo por el que se busca
--- movie/views.py
+++ movie/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Movie
 
 def home(request):
     return render(request, "home.html", {"name": "Juan Esteban"})
@@ -7,13 +8,3 @@ def about(request):
     return render(request, "about.html")
 
 
-from django.shortcuts import render
-
-def home(request):
-    return render(request, "home.html")
-
-def about(request):
-    return render(request, "about.html")
-
-
-    
